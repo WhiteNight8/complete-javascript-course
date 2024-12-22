54ecc2f9c621c69ffedab1538df3752830c7f299
Xu <whitenight.xiaxu@gmail.com>
2024-12-22 15:55:09 +0800

    Update script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,11 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.eu/rest/v2/name/portugal`);
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});

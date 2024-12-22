5b1d4a8c5a02de29767696cfbc36790297b7a2db
Xu <whitenight.xiaxu@gmail.com>
2024-12-22 16:47:50 +0800

    cors

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -6,7 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.eu/rest/v2/name/portugal`);
+request.open('GET', `https://restcountries.com/v3.1/name/eesti`);
 request.send();
 
 request.addEventListener('load', function () {

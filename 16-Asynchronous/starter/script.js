6989d1bf270d6760b6ef58b252e9843cb2b2772e
Xu <whitenight.xiaxu@gmail.com>
2024-12-28 17:18:46 +0800

    Update script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,11 +4,38 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.com/v3.1/name/eesti`);
-request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const flags = data.flags.png;
+    const name = data.name.common;
+    const region = data.region;
+    const population = data.population;
+    const languages = Object.entries(data.languages)[0][0];
+    const currency = Object.entries(data.currencies)[0][0];
 
-request.addEventListener('load', function () {
-  console.log(this.responseText);
-});
+    const html = `
+   <article class="country">
+          <img class="country__img" src="${flags}" />
+          <div class="country__data">
+            <h3 class="country__name">${name}</h3>
+            <h4 class="country__region">${region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currency}</p>
+          </div>
+        </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('portugal');
+getCountryData('usa');
